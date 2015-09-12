c2992c25a6f17c151e67ce46c441b172a7f2b783
Wes Pickett <wes@krossover.com>
2015-09-11 22:40:28 -0400

    make extension reuse singleton tab when icon is clicked when the tab is already open.

--- chrome-extension/scripts.js
+++ chrome-extension/scripts.js
@@ -1,3 +1,20 @@
+var activeTab;
+var created = false;
+
+chrome.tabs.onRemoved.addListener(function(closedTabId) {
+    if (activeTab && closedTabId === activeTab.id) {
+        created = false;
+        activeTab = void(0);
+    }
+});
+
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.create({url: chrome.extension.getURL('time-tracking-page.html')});
+    if (!created) {
+        created = true;
+        chrome.tabs.create({url: chrome.extension.getURL('time-tracking-page.html')}, function(createdTab) {
+            activeTab = createdTab;
+        });
+    } else {
+        chrome.tabs.update(activeTab.id, {selected: true});
+    }
 });
